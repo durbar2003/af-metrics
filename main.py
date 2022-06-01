a0b809f212fc160210b584ef08551f4bc0c5ef86
durbar chakrabarty <durbardibyo@gmail.com>
2022-06-01 22:41:28 +0530

    modified file structure

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ from prometheus_client import start_http_server, Summary
 import random
 import time
 
-# Create a metric to track time spent and requests made.
 REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
 
-# Decorate function with metric.
 @REQUEST_TIME.time()
 def process_request(t):
     """add()"""
