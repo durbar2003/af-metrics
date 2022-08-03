28b08b7a7b922fef22f6b583b70a1911209ceb04
durbar2003 <durbardibyo@gmail.com>
2022-08-03 21:54:47 +0530

    added monitoring python file

--- docs/main.py
+++ docs/main.py
@@ -1,16 +1,42 @@
-from prometheus_client import start_http_server, Summary
-import random
 import time
+from prometheus_client.core import GaugeMetricFamily, REGISTRY, CounterMetricFamily
+from prometheus_client import start_http_server
+import requests
+import json
 
-REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+class CustomCollector(object):
+    def __init__(self):
+        pass
 
-@REQUEST_TIME.time()
-def process_request(t):
-    time.sleep(t)
+    def collect(self):
+        d1 = {
+            "coffea_notebook_metric": [
+                {
+                "metric": {"bytesread":2076921210},
+                "value":0
+                },
+                {
+                "metric": {"entries":53446198},
+                "value":0
+                },
+                {
+                "metric":{"processtime":741.4781568050385},
+                "value":0
+                },
+                {
+                "metric":{"chunks":267},
+                "value":0
+                }
+            ]
+        }
+        list_of_metrics = d1["coffea_notebook_metric"]
+        for key in list_of_metrics:
+           g = GaugeMetricFamily("Coffea_Casa_Metrics", 'Custom_facility_metrics', labels=['coffea_casa_metrics'])
+           g.add_metric([str(key['metric'])], key['value'])
+           yield g
 
 if __name__ == '__main__':
-    # Start up the server to expose the metrics.
-    start_http_server(8000)
-    # Generate some requests.
+    start_http_server(8003)
+    REGISTRY.register(CustomCollector())
     while True:
-        process_request(random.random())
+        time.sleep(1)
