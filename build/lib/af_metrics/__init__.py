44de3347c2f1145b537617b3f79d600fe1e5a66c
durbar chakrabarty <durbardibyo@gmail.com>
2022-08-05 11:10:07 +0530

    build dependenies

---
+++ build/lib/af_metrics/__init__.py
@@ -0,0 +1,12 @@
+"""
+Copyright (c) 2022 Durbar Chakraborty. All rights reserved.
+
+af-metrics: Source code for analysis of metrics on Coffea-Casa Analysis Facility
+"""
+
+
+from __future__ import annotations
+
+from ._version import version as __version__
+
+__all__ = ("__version__",)
