ff252ca47c56f2e28aa8f954f1078d97a6985469
durbar chakrabarty <durbardibyo@gmail.com>
2022-08-07 03:35:26 +0530

    bundling modules

--- build/lib/af_metrics/__init__.py
+++ build/lib/af_metrics/__init__.py
@@ -8,8 +8,10 @@ af-metrics: Source code for analysis of metrics on Coffea-Casa Analysis Facility
 from __future__ import annotations
 
 from ._version import version as __version__
+# from .monitoring_module import CustomCollector
 
 __all__ = ("__version__",)
 
 __version__="0.1.0"
 __author__='Durbar Chakraborty'
+# __core__=CustomCollector()
